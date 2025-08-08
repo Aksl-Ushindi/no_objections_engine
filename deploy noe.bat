bec17c833694720e1d130cd8079caf01e3db6c96
Aksl-Ushindi <ton_email_github@exemple.com>
2025-08-08 10:46:37 +0200

    Update

--- deploy noe.bat
+++ deploy noe.bat
@@ -1,40 +1,18 @@
 @echo off
-REM === Script de push et déploiement du No Objections Engine ===
-
-REM Nom du dépôt distant GitHub (à personnaliser)
-set REPO_URL=https://github.com/Aksl-Ushindi/no_objections_engine.git
-set BRANCH=main
-
-REM Aller dans le dossier du script
+chcp 65001 >nul
 cd /d "%~dp0"
 
-REM Initialiser Git si besoin
-if not exist .git (
-    echo Initialisation du dépôt Git...
-    git init
-    git branch -M %BRANCH%
-    git remote add origin %REPO_URL%
-) else (
-    echo Dépôt Git déjà initialisé.
-)
+REM --- CONFIG ---
+set "USER=Aksl-Ushindi"
+set "REPO=no_objections_engine"
+set "BRANCH=main"
+REM -------------
 
-REM Ajouter tous les fichiers
-echo Ajout des fichiers...
+git init
+git branch -M %BRANCH%
+git remote add origin https://github.com/%USER%/%REPO%.git
 git add .
-
-REM Commit avec date/heure
-set TIMESTAMP=%date% %time%
-git commit -m "Mise à jour automatique %TIMESTAMP%"
-
-REM Push vers GitHub
-echo Envoi vers GitHub...
+git commit -m "Update"
 git push -u origin %BRANCH%
 
-REM Message final
-echo.
-echo =======================================
-echo ✅ Déploiement terminé !
-echo Vérifie sur GitHub Pages si activé :
-echo https://Aksl-Ushindi.github.io/no_objections_engine/
-echo =======================================
 pause
